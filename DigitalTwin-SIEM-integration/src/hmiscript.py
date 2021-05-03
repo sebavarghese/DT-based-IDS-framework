c21b66534eab51d949c5b3dc38c08e8764745441
root <root@seba-VirtualBox>
2021-05-03 11:06:04 +0200

    False data injection

--- DigitalTwin-SIEM-integration/src/hmiscript.py
+++ DigitalTwin-SIEM-integration/src/hmiscript.py
@@ -6,11 +6,12 @@ from minicps.devices import HMI
 from utils import STATE, PLC1_ADDR
 from utils import HMI_PROTOCOL, HMI_DATA, HMI_ADDR, HMI_PERIOD_SEC
 
+import sys
 import time
 import logging
 
 ACTUATOR1 = ('ACTUATOR1-MV', 1) # to be received from PLC1 
-ACTUATOR1_HMI = ('ACTUATOR1-HMI', 1) # to be sent to PLC1
+#ACTUATOR1_HMI = ('ACTUATOR1-HMI', 1) # to be sent to PLC1
 
 class FPHMI(HMI):
 
@@ -25,11 +26,14 @@ class FPHMI(HMI):
         logging.basicConfig(filename='logs/hmi.log',
                             format='%(levelname)s %(asctime)s ' + HMI_ADDR + ' %(funcName)s %(message)s',
                             datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
-        while(True):
-            self.set(ACTUATOR1, 0)
-            self.send(ACTUATOR1, 0, PLC1_ADDR)
-        
-            time.sleep(180)
+        delay=60*int(sys.argv[1])
+        close_time=time.time()+delay
+        while close_time > time.time():
+            motor_status = int(self.receive(ACTUATOR1, PLC1_ADDR))
+            toggle_status = int(not motor_status)
+            self.set(ACTUATOR1, toggle_status)
+            self.send(ACTUATOR1, toggle_status, PLC1_ADDR)
+            time.sleep(10)
 
 if __name__ == "__main__":
 
