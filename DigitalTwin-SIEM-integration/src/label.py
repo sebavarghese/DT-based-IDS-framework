85130316772e9ff4580ba189841ffb61f3dc8d4b
root <root@seba-VirtualBox>
2021-06-21 22:11:36 +0200

    update

--- DigitalTwin-SIEM-integration/src/label.py
+++ DigitalTwin-SIEM-integration/src/label.py
@@ -17,15 +17,15 @@ with open('logs/data.csv','r') as csvinput:
                 ### Anomaly when motor valve closed when bottle_ll < bottle_ub and when tank_ll > tank_lb
                 ### Anomaly when motor valve is open if bottle_ll >= bottle_ub or if flowlevel >= sensor2_threshold or if tank_ll <= tank_ll
                 ############################################################################################################################
-                if int(row[9]) == 1 and (float(row[1]) <= tank_lb or (float(row[4]) >= SENSOR2_THRESH and float(row[4]) != 999) or (float(row[6]) >= bottle_ub and float(row[6]) != 999)):
+                if int(row[4]) == 1 and (float(row[1]) <= tank_lb or (float(row[2]) >= SENSOR2_THRESH and float(row[2]) != 999) or (float(row[3]) >= bottle_ub and float(row[3]) != 999)):
                     writer.writerow(row+['False data injection'])
-                elif int(row[9]) == 0 and float(row[6]) < bottle_ub and float(row[1]) > tank_lb:
+                elif int(row[4]) == 0 and float(row[3]) < bottle_ub and float(row[1]) > tank_lb:
                     writer.writerow(row+['False data injection'])
                 ###########################################################
                 ### For MitM/DoS attacks as well as TCP SYN flood attacks
                 ### Anomaly if flowlevel or bottle_ll is marked as 999
                 ###########################################################
-                elif float(row[4]) == 999 or float(row[6]) == 999:
+                elif float(row[2]) == 999 or float(row[3]) == 999:
                     writer.writerow(row+['DoS'])
                 
 
