55ff0093d1432516e7f300668c523fad1037b1a1
sebavarghese <62105008+sebavarghese@users.noreply.github.com>
2021-07-12 09:56:01 +0200

    Update run.py

--- DigitalTwin-SIEM-integration/src/run.py
+++ DigitalTwin-SIEM-integration/src/run.py
@@ -25,21 +25,7 @@ class FPCPS(MiniCPS):
 
         # start devices
         plc1, plc2, plc3, s1, attacker, hmi = self.net.get('plc1', 'plc2', 'plc3', 's1', 'attacker', 'hmi')
-        '''
-        s1.cmd('screen -dmSL tank python physical_process.py')
-        s1.cmd('screen -dmSL bottle python physical_process_bottle.py')
-        plc3.cmd('screen -dmSL plc3 python plc3.py -Logfile')
-        plc2.cmd('screen -dmSL plc2 python plc2.py -Logfile')
-        plc1.cmd('screen -dmSL plc1 python plc1.py -Logfile')
-        attacker.cmd('screen -dmSL attacker bash attack.sh')
-        
-        net.terms += makeTerm(s1, display=None, cmd='wireshark -i s1-eth1 -a duration:180 -k -w pcaps/cap1.pcap')
-        net.terms += makeTerm(s1, display=None, cmd='wireshark -i s1-eth2 -a duration:180 -k -w pcaps/cap2.pcap')
-        net.terms += makeTerm(s1, display=None, cmd='wireshark -i s1-eth3 -a duration:180 -k -w pcaps/cap3.pcap')
-        net.terms += makeTerm(s1, display=None, cmd='wireshark -i s1-eth4 -a duration:180 -k -w pcaps/cap4.pcap')
-        net.terms += makeTerm(s1, display=None, cmd='wireshark -i s1-eth5 -a duration:180 -k -w pcaps/cap5.pcap')
-        ''' 
-        # uncomment the following lines (while removing the .cmd lines above)
+
         net.terms += makeTerm(s1, display=None, cmd='python physical_process.py')
         time.sleep(0.2)
         net.terms += makeTerm(s1, display=None, cmd='python physical_process_bottle.py')
