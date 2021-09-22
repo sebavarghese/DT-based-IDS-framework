d5efd9b8e65e449726e582dd5b1d44ef63157c73
sebavarghese <62105008+sebavarghese@users.noreply.github.com>
2021-09-22 17:12:46 +0200

    Update label1.py

--- DigitalTwin-SIEM-integration/src/label1.py
+++ DigitalTwin-SIEM-integration/src/label1.py
@@ -1,5 +1,5 @@
 ##### Script to do second level of automated labelling of dataset from level 1, i.e., label.py #####
-##### Takes 3 command line arguments: 1) Start time of attack 2) End time of attack 3)  Label: Scaling, Data tampering etc #####
+##### Takes 3 command line arguments: 1) Start time of attack 2) End time of attack 3)  Label: Calculated measurement injetcion, Naive measurement injection etc #####
 ##### Example: run 'python label1.py '2021-07-01 16:04:07.681880' '2021-07-01 16:05:11.900075' Scaling #####
 import csv
 import math
